1b2983d1647069bd33a2d1a43af3375a450456ef
Dalibor Pastorek <81307109+DaliborPsatorek@users.noreply.github.com>
2022-12-09 13:01:59 +0100

    Add files via upload

---
+++ Nuxt-app/app.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<div class="title">Counter App</div>
+		<div class="counter-continer">
+			<div class="box" v-on:click="minus()">-</div>
+			<div class="box" v-if="counter != 0">{{ counter }}</div>
+			<div class="box" v-else>Zero</div>
+			<div class="box" v-on:click="plus()">+</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	console.clear()
+	let counter = ref(0)
+	const plus = () => {
+		counter.value += 1
+	}
+	const minus = () => {
+		counter.value -= 1
+	}
+</script>
+
+<style>
+	.title {
+		font-size: 5rem;
+		margin-bottom: 2rem;
+	}
+	body {
+		background-color: rgb(14, 239, 255);
+		display: flex;
+		justify-content: center;
+	}
+	.counter-continer {
+		display: flex;
+		justify-content: space-between;
+	}
+	.box {
+		background-color: rgb(0, 89, 255);
+		font-size: xx-large;
+		color: rgb(206, 205, 205);
+		height: 3rem;
+		padding-left: 1rem;
+		padding-right: 1rem;
+		width: auto;
+		text-align: center;
+	}
+</style>
